624c3a1945dacfb81b05239e55b011d19ba895cb
Yevhen_Kalashnykov <ykalashnykov@corelogic.com>
2021-10-09 08:33:09 -0700

    simple structure is added

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LogoComponent } from './component/logo/logo.component';
+import { CoursesPageComponent } from './component/courses-page/courses-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/model/course-typescript.ts
@@ -0,0 +1,7 @@
+export interface CourseTypescript {
+  id: string;
+  title: string;
+  creationDate: Date;
+  duration: number;
+  description: string;
+}
